c736674ef758f38f88f80abe1558799744b4d9da
sejun <dailyzett@gmail.com>
2024-10-14 21:29:37 +0900

    fix: 플로우 생명주기 함수로 코드 변경

--- src/main/kotlin/com/example/paymentdemo/service/PaymentService.kt
+++ src/main/kotlin/com/example/paymentdemo/service/PaymentService.kt
@@ -7,10 +7,7 @@ import com.example.paymentdemo.domain.jpo.EventJpo
 import com.example.paymentdemo.domain.jpo.EventKey
 import com.example.paymentdemo.domain.jpo.EventsJpoRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.flowOn
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import org.slf4j.LoggerFactory.getLogger
@@ -34,18 +31,25 @@ class PaymentService(
         mutex.withLock {
             val currentTime = LocalDateTime.now(clock)
             val key = EventKey(currentTime, EnumEventType.PAYMENT, command.username)
-
-            log.info("현재 시간:{}", currentTime)
-
+            emit(key to command.amount)
+        }
+    }.transformWhile { (key, amount) ->
             val paymentEventCount = eventsJpoRepository.countById(key)
             if (paymentEventCount > 0) {
                 log.info("중복 이벤트 발생")
+                false
             } else {
-                eventsJpoRepository.save(EventJpo(key, command.amount))
-                val event = PaymentEvent(command.username, command.amount)
-                emit(event)
+                emit(key to amount)
+                true
             }
         }
-    }.flowOn(Dispatchers.IO)
+        .onEach { (key, amount) -> eventsJpoRepository.save(EventJpo(key, amount)) }
+        .map { (_, amount) -> PaymentEvent(command.username, amount) }
         .onEach { userService.subtractUserWallet(it) }
+        .catch { e ->
+            log.error("Error occurred while processing payment: ${e.message}", e)
+            throw e
+        }
+        .flowOn(Dispatchers.IO)
+
 }
